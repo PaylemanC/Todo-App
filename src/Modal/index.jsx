6d2fc6c6992f4567f931643d54a3be2c2d33f5b9
Carina Payleman <rociocarina2@gmail.com>
2024-04-13 14:15:23 -0300

    Estiliza modal

--- src/Modal/index.jsx
+++ src/Modal/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { TbSquareXFilled } from "react-icons/tb";
+import { ModalContext } from '../contexts/ModalContext';
+import './Modal.css';
 
 function Modal({ children }) {
+    const { openModal, setOpenModal } = React.useContext(ModalContext);
+
     return ReactDOM.createPortal( 
-        <div className="modal">
-            { children }
+        <div className="modal-background">
+            <div className="modal-container">
+                <button className="modal-close-btn"
+                    onClick={() => {
+                        setOpenModal(!openModal);
+                    }}>
+                    <TbSquareXFilled/>
+                </button>
+                <div className="modal-content">
+                    { children }
+                </div>
+            </div>
         </div>,
         document.getElementById('modal')
     );
